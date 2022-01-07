d5c286864ccf5616af21daf0cb915cdd61f5614f
pratik0311 <pratikpatel0311@gmail.com>
2022-01-07 16:41:54 +0530

    updated menuitems with naviagtion router-dom

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import withRouter from '../../components/withRouter.component.jsx';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
-        <div className="background-image" style={{ 
-            backgroundImage: `url(${imageUrl})`
-        }}/>
+const MenuItem = ({ title, imageUrl, size, linkUrl, router }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => router.navigate(`${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
-export default MenuItem;
+export default withRouter(MenuItem);
